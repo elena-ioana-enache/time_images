4508b118ed2d644210472ca72f33b8002c1a21a2
Elena Enache <elena.ioana.enache@gmail.com>
2022-12-27 17:50:43 +0200

    Add app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const date = new Date().toDateString();
+  const [hour, setHour] = useState(new Date().toTimeString());
+  const [img, setImg] = useState('');
+  const [contor, setContor] = useState(0);
+  const [stopFetch, setStopFetch] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setHour(new Date().toTimeString())
+    }, 1000);
+  })
+  useEffect(() => {
+    if (!stopFetch) {
+      fetch('https://picsum.photos/200')
+        .then(response =>
+          setImg(response.url));
+    }
+  }, [contor, stopFetch]);
+  useEffect(() => {
+    setTimeout(() => {
+      setContor(contor + 1);
+    }, 4000);
+  })
+  const onImageClick = () => {
+    setStopFetch(!stopFetch)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{`${date} ${hour}`}</div>
+      <img src={img} style={{ width: '200px', height: '200px' }} onClick={onImageClick} />
     </div>
   );
 }
